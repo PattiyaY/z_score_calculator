de1482aab53620a1202181c3226efe82868f0de0
Jan Poonthong <janpoonthong628@gmail.com>
2023-08-18 23:08:25 +0700

    fix: manually copy files to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname),
   },
   mode: "development",
   module: {
@@ -21,5 +21,15 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+        { from: "src/index.html", to: "dist/index.html" },
+        { from: "src/index.css", to: "dist/index.css" },
+        { from: "assets/107806435.png", to: "dist/assets/107806435.png" },
+        { from: "bundle.js", to: "dist/bundle.js" },
+        ],
+      }),
+    ],
 };
