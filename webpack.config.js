cde1179e15edc995a5f3706fa7be0247cd34e988
Jan Poonthong <janpoonthong628@gmail.com>
2023-08-20 09:17:15 +0700

    fix: build and deploy on path and linking bundle and assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,49 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./index.js",
+const commonConfig = {
+  entry: "./src/index.js",
+  mode: "development",
+};
+
+const distConfig = {
+  ...commonConfig,
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
   module: {
     rules: [
       {
         test: /107806435\.png$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: 'assets/[name].[ext]',
-            }
-          }
-        ]
-      }
-    ]
+              name: "assets/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-        { from: "src/index.html", to: "dist/index.html" },
-        { from: "src/index.css", to: "dist/index.css" },
-        { from: "assets/107806435.png", to: "dist/assets/107806435.png" },
-        { from: "bundle.js", to: "dist/bundle.js" },
-        ],
-      }),
-    ],
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/index.html", to: "index.html" },
+        { from: "src/index.css", to: "index.css" },
+        { from: "src/assets/107806435.png", to: "assets/107806435.png" },
+      ],
+    }),
+  ],
 };
+
+const srcConfig = {
+  ...commonConfig,
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "src"),
+  },
+};
+
+module.exports = [distConfig, srcConfig];
